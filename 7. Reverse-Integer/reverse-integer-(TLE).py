5e0a98ea229d5c8df071fa8cdd600e889d044107
Saonee Nandi <saonee.nandi@gmail.com>
2026-05-25 23:20:45 +0530

    Delete 7. Reverse-Integer directory

--- 7. Reverse-Integer/reverse-integer-(TLE).py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-        def reverse(self, x):
-            num=0
-            while (x%10)!=0:
-                digit = x%10
-                num=digit+10*num
-                x//10
-            return num
