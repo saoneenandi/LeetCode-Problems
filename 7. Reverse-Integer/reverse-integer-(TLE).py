077eb11079a31702014c0d6d472bfc847a4fb91f
Saonee Nandi <saonee.nandi@gmail.com>
2026-05-25 22:49:20 +0530

    Refactor reverse method for integer reversal
    Fix indentation and correct integer division in reverse method.

--- 7. Reverse-Integer/reverse-integer-(TLE).py
+++ 7. Reverse-Integer/reverse-integer-(TLE).py
@@ -1,9 +1,8 @@
 class Solution(object):
-    def reverse(self, x):
-      num=0
-        while (x%10)!=0:
-            digit = x%10
-            num=digit+10*num
-            num=(num/10)
-            int(num)
-        return num
+        def reverse(self, x):
+            num=0
+            while (x%10)!=0:
+                digit = x%10
+                num=digit+10*num
+                x//10
+            return num
