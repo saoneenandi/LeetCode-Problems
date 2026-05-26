32e5e1294dfd94b7dd31f8450d4caf604039e88e
Saonee Nandi <saonee.nandi@gmail.com>
2026-05-26 10:25:07 +0530

    Time: 0 ms (100%), Space: 12.3 MB (64.92%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def myPow(self, x, n):
+        if n < 0:
+            x = 1 / x
+            n = -n
+        result = 1
+        current_product = x
+        while n > 0:
+            if n % 2 == 1:
+                result *= current_product
+            current_product *= current_product
+            n //= 2
+        return result
+
+        
