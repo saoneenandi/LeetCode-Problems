4ebc92563e21c3c8216121f43dfac751cefbc7ca
Saonee Nandi <saonee.nandi@gmail.com>
2026-06-01 22:59:45 +0530

    Time: 4 ms (15.63%), Space: 12.5 MB (18.75%) - LeetHub

---
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def minimumCost(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        cost.sort(reverse=True)
+        ans = 0
+        for i in range(len(cost)):
+            if i % 3 != 2:
+                ans += cost[i]
+        return ans
